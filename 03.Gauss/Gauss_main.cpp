1b14db073297502c07e67fef742c76f69dbee61c
Max <babaevmaksim2007@gmail.com>
2026-06-05 19:41:51 +0300

    CI_modernisation

--- 03.Gauss/Gauss_main.cpp
+++ 03.Gauss/Gauss_main.cpp
@@ -4,21 +4,26 @@
 
 int main(int argc, const char *argv[])
 {
-    if (argc < 2) {
+    if (argc < 2)
+    {
         std::cerr << "Использование: " << argv[0] << " <input.csv>\n";
         return 1;
     }
 
-    try {
+    try
+    {
         GaussMatrix ab = load_csv_to_matrix(argv[1]);
         
         GaussVector x = Gauss_solve(ab);
 
         std::cout << "x\n";
-        for (int i = 0; i < x.size(); ++i) {
+        for (int i = 0; i < x.size(); ++i)
+        {
             std::cout << x(i) << "\n";
         }
-    } catch (const std::exception& e) {
+    }
+    catch (const std::exception& e)
+    {
         std::cerr << "Ошибка: " << e.what() << "\n";
         return 1;
     }
